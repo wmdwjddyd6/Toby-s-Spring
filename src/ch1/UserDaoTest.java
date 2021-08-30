ed3e0f996441ded84128b76a33f73c6cd952b183
BlueEar <67060302+wmdwjddyd6@users.noreply.github.com>
2021-08-30 20:15:29 +0900

    Spring Bean
    Bean 등록

--- src/ch1/UserDaoTest.java
+++ src/ch1/UserDaoTest.java
@@ -5,17 +5,20 @@ import ch1.springbook.user.dao.DConnectionMaker;
 import ch1.springbook.user.dao.DaoFactory;
 import ch1.springbook.user.dao.UserDao;
 import ch1.springbook.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String args[]) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+        user.setId("ID1");
+        user.setName("김광민");
+        user.setPassword("kkm1234");
 
         dao.add(user);
 
--- src/ch1/springbook/user/dao/DaoFactory.java
+++ src/ch1/springbook/user/dao/DaoFactory.java
@@ -1,6 +1,12 @@
 package ch1.springbook.user.dao;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
@@ -13,6 +19,7 @@ public class DaoFactory {
         return new MessageDao(connectionMaker());
     }
 */
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
